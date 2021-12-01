74001e7eecf8843ab23bccaec4ab3b5190616f32
sjw3957 <sjw3957@gmail.com>
2021-12-01 21:31:49 +0900

    4.1 Form Submission

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick(){
-    console.log("hello", loginInput.value);
+    const username = loginInput.value;
+    console.log(username);
 }
 
 loginButton.addEventListener("click",onLoginBtnClick);
