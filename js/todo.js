b63e7e7cf82b8e3e09edba9c6841693fda83ec2e
sjw3957 <sjw3957@gmail.com>
2021-12-05 02:29:34 +0900

    7.1 Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
