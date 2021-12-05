fe8ba9ee5440c70f59824154f5087b35152cfc25
sjw3957 <sjw3957@gmail.com>
2021-12-05 13:22:22 +0900

    7.3 Saving To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos"
 const toDos = [];
 
 function saveToDos(){
-    localStorage.setItem("todos",JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
-
 
 function deleteToDo(event){
     const li = event.target.parentElement;
@@ -37,3 +37,10 @@ function handleToDoSubmit(event){
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(saveToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) => console.log(item));
+}
