da5d96ca1599ac9b5158da2c990f6583d25fa086
sjw3957 <sjw3957@gmail.com>
2021-12-05 19:51:05 +0900

    8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY="99059ab7d5d5081f2ba88042be412a7d";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("you live it",lat,lng);
+    const lon = position.coords.longitude;
+    console.log("you live it",lat,lon);
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+        const weather= document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        city.innerText = data.name;
+    });
 }
 
 function onGeoError(){
